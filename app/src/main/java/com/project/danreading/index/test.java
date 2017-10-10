417bf2c5b2138aea9d37bd52d9605c4e475f337b
lqren <3262517052@qq.com>
2017-10-10 23:27:01 +0800

    update project

--- app/src/main/java/com/project/danreading/index/test.java
+++
@@ -1,8 +0,0 @@
-package com.project.danreading.index;
-
-/**
- * Created by Administrator on 2017/10/10.
- */
-
-public class test {
-}
--- app/src/main/java/com/project/danreading/job/test.java
+++
@@ -1,8 +0,0 @@
-package com.project.danreading.job;
-
-/**
- * Created by Administrator on 2017/10/10.
- */
-
-public class test {
-}
